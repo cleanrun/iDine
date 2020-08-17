b7cb1aa612334c6a5075c532061805224a5e5c9c
Marchell <62990473+marchellwgs@users.noreply.github.com>
2020-08-17 11:13:13 +0700

    initial commit

---
+++ iDine/OrderView.swift
@@ -0,0 +1,50 @@
+//
+//  OrderView.swift
+//  iDine
+//
+//  Created by Marchell on 16/08/20.
+//  Copyright © 2020 Marchell. All rights reserved.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    @EnvironmentObject var order: Order
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    ForEach(order.items) { item in
+                        HStack {
+                            Text(item.name)
+                            Spacer()
+                            Text("$\(item.price)")
+                        }
+                    }.onDelete(perform: deleteItems)
+                }
+                
+                Section {
+                    NavigationLink(destination: CheckOutView()) {
+                        Text("Place Order")
+                    }
+                }.disabled(order.items.isEmpty)
+            }
+            .navigationBarTitle("Order")
+            .listStyle(GroupedListStyle())
+            .navigationBarItems(trailing: EditButton())
+        }
+    }
+    
+    func deleteItems(at offsets: IndexSet) {
+        order.items.remove(atOffsets: offsets)
+    }
+}
+
+struct OrderView_Previews: PreviewProvider {
+    static let order = Order()
+    
+    static var previews: some View {
+        OrderView().environmentObject(order)
+    }
+}
